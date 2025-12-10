d714819db5d561701146b8b23a72fc6168406ea9
Nanshan-Q <71011287+Nanshan-Q@users.noreply.github.com>
2025-12-10 13:07:44 +0800

    Update and rename coolapk.js to AD.js

--- AD.js
+++ AD.js
@@ -1,5 +1,10 @@
 #!name=自用模块
 #!desc=2025-12-10
+
+ [Header Rewrite]
+# if-none-match返回304状态码
+http-request ^https:\/\/(spclient\.wg\.spotify\.com|.*-spclient\.spotify\.com(:443)?)\/user-customization-service\/v1\/customize$ header-del if-none-match
+ 
 [Script]
 ###》酷安去广告
 # > 酷安_开屏广告@ddgksf2013
@@ -17,7 +22,10 @@ coolapk-hotsearch =type=http-response, pattern=^https?:\/\/api\.coolapk\.com\/v6
 # > 酷安_酷品页推广@ddgksf2013
 coolapk-page-datalist =type=http-response, pattern=https:\/\/api\.coolapk\.com\/v6\/page\/dataList\?.*title=%E9%85, script-path=https://github.com/ddgksf2013/Scripts/raw/master/coolapk.js, requires-body=true
 
-
+###》Spotify
+ spotify-json = type=http-request,pattern=^https:\/\/(spclient\.wg\.spotify\.com|.*-spclient\.spotify\.com(:443)?)\/(artistview\/v1\/artist|album-entity-view\/v2\/album)\/,requires-body=0,script-path=https://raw.githubusercontent.com/app2smile/rules/master/js/spotify-json.js
+spotify-proto = type=http-response,pattern=^https:\/\/(spclient\.wg\.spotify\.com|.*-spclient\.spotify\.com(:443)?)\/(bootstrap\/v1\/bootstrap|user-customization-service\/v1\/customize)$,requires-body=1,binary-body-mode=1,max-size=0,script-path=https://raw.githubusercontent.com/app2smile/rules/master/js/spotify-proto.js,script-update-interval=0
+spotify歌词翻译 = type=http-response,pattern=^https:\/\/spclient\.wg\.spotify\.com\/color-lyrics\/v2\/track\/,requires-body=1,binary-body-mode=1,max-size=0,script-path=https://raw.githubusercontent.com/app2smile/rules/master/js/spotify-lyric.js,enable={{{歌词翻译}}},argument=appid={{{翻译账户}}}&securityKey={{{翻译密钥}}}
 
  ###》百度贴吧
   # 贴吧 JSON 广告去除规则
@@ -30,7 +38,8 @@ tieba-proto =type=http-response, pattern=^http(s:\/\/tiebac|:\/\/c\.tieba)\.baid
 
 
 
-hostname = %APPEND% api.coolapk.com, tiebac.baidu.com
+hostname = %APPEND% api.coolapk.com, tiebac.baidu.com, spclient.wg.spotify.com, *spclient.spotify.com
+
 
 
 
