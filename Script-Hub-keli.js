22c62cee2d8ef8302618b088c9667ceabe12c8a8
Nanshan-Q <71011287+Nanshan-Q@users.noreply.github.com>
2025-12-18 13:00:29 +0800

    Update Script-Hub-keli.js

--- Script-Hub-keli.js
+++ Script-Hub-keli.js
@@ -1,14 +1,12 @@
 #!name=Loon模块转换
 #!desc=将可莉插件中心转换给小火箭使用
   
-[General]
-force-http-engine-hosts = %APPEND% script.hub, *.script.hub
-
 [Body Rewrite]
-^https:\/\/(hub|pluginhub)\.kelee\.one\/list\.json "loon:\/\/import\?plugin=https:\/\/kelee\.one\/Tool\/Loon\/Lpx\/(.+)\.lpx" "shadowrocket:\/\/install?module=http:\/\/script.hub\/file\/_start_\/hub\/_end_\/$1.sgmodule?type=loon-plugin&target=shadowrocket-module&del=true&jqEnabled=true"
+http-response ^https://(hub|pluginhub)\.kelee\.one/list\.json "loon://import\?plugin=(https://kelee.one/Tool/Loon/Lpx/(.+).lpx)" "shadowrocket://install?module=http://script.hub/file/_start_/$1/_end_/$2.sgmodule%3Ftype%3Dloon-plugin%26target%3Dshadowrocket-module%26del%3Dtrue%26jqEnabled%3Dtrue"
 
 [MITM]
-hostname = %APPEND% script.hub, *.script.hub, hub.kelee.one, pluginhub.kelee.one
+hostname = %APPEND% hub.kelee.one, pluginhub.kelee.one
+
 
 
 
